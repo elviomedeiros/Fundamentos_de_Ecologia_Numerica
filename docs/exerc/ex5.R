d8d77c8bb094b360f0acc51e7e0b5765954529ed
elviomedeiros <elviomedeiros@servidor.uepb.edu.br>
2026-08-02 16:01:08 -0300

    detalhes correg e regmul

--- docs/exerc/ex5.R
+++ docs/exerc/ex5.R
@@ -6,4 +6,16 @@ e peso como variável dependente.
 Faça uma análise de regrassão simples do 
 comprimento total e peso corporal em peixes.
 
+Apresente:
 
+A pergunta a ser respondida.
+
+As hipóteses que a regressão testa (H0/H1).
+
+Os resultados da regressão que o R apresenta.
+
+O gráfico da regressão com a linha de tendência.
+
+A equação da regressão.
+
+Um texto conclusivo no formato de "O QUE REPORTAR".
