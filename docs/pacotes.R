1076d70afc7d92ed5ea348b1baeed6c8402974e2
elviomedeiros <elviomedeiros@servidor.uepb.edu.br>
2026-08-03 11:19:00 -0300

    pacotes e glms

---
+++ docs/pacotes.R
@@ -0,0 +1,81 @@
+##INSTALE O R ----
+##INSTALE O RSTUDIO ----
+
+##INSTALE OS SEGUINTES PACOTES ----
+
+###CAPÍTULO 1.1 ----
+
+install.packages("knitr")
+
+###CAPÍTULO 3 ----
+
+install.packages("tibble")
+install.packages("gt")
+install.packages("knitr")
+install.packages("kableExtra")
+
+###CAPÍTULO 4.2 ----
+
+install.packages("readxl")
+install.packages("openxlsx")
+install.packages("readODS")
+install.packages("tidyverse")
+install.packages("dplyr")
+
+###CAPÍTULO 4.3.1 ----
+
+install.packages("fdth")
+install.packages("moments")
+install.packages("nortest")
+
+###CAPÍTULO 4.3.2 ----
+
+install.packages("ggpubr")
+install.packages("MASS")
+install.packages("car")
+install.packages("gplots")
+install.packages("multcomp")
+install.packages("FSA")
+install.packages("rcompanion")
+
+###CAPÍTULO 4.4.1 ----
+
+install.packages("ppcor")
+install.packages("ggplot2")
+
+###CAPÍTULO 4.4.2 ----
+
+install.packages("psych")
+install.packages("corrplot")
+install.packages("vegan")
+install.packages("caret")
+install.packages("leaps")
+
+###CAPÍTULO 4.4.3 ----
+
+packageVersion("rlang")
+remove.packages("rlang")
+install.packages("rlang")
+packageVersion("rlang") #atualizar para a versão 1.3.0
+install.packages("devtools")
+devtools::install_github("dustinfife/flexplot")
+install.packages("effectsize")
+install.packages("flexplot")
+install.packages("ggplot2")
+
+###CAPÍTULO 4.5.1 ----
+
+install.packages("tibble")
+install.packages("tidyverse")
+install.packages("forcats")
+install.packages("Rcpp")
+install.packages("tidyr")
+install.packages("iNEXT")
+
+###CAPÍTULO 4.5.2 ---- ESSE NÃO PRECISA
+
+install.packages("eulerr")
+install.packages("VennDiagram")
+install.packages("ggvenn")
+install.packages("ggVennDiagram")
+install.packages("gplots")
