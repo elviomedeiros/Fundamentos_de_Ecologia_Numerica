5e7fd379dbdfeaff7ebd1424219ef908fbc523fc
elviomedeiros <elviomedeiros@servidor.uepb.edu.br>
2026-08-05 14:43:38 -0300

    formulas correg finalizadas

--- docs/exerc/ex6-simpregres.R
+++ docs/exerc/ex6-simpregres.R
@@ -1,9 +1,29 @@
 ##### GRUPO TODOS #####
 Em estudos de relação peso-comprimento normalmente
+necessita-se comparar variáveis morfológicas.
+
+Faça a correlação entre co CT_cm e PT_g.
+
+Apresente:
+  
+A sua pergunta a ser respondida.
+
+As hipóteses que sua correlação testa (H0/H1).
+
+Os resultados da regressão que o R apresenta,
+destacando o valor de r e de p.
+
+O gráfico da correlação.
+
+Um texto conclusivo no formato de "O QUE REPORTAR".
+
+
+##### GRUPO TODOS #####
+Em estudos de relação peso-comprimento normalmente
 considera-se comprimento como variável independente
 e peso como variável dependente.
 
-Faça uma análise de regrassão simples do 
+Faça uma análise de regressão simples do 
 comprimento total e peso corporal em peixes.
 
 Apresente:
